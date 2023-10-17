394ea1ba3c393a1249eada080459766660cb7e64
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-10-18 00:01:37 +0530

    tailwind css and daisyui installed

---
+++ src/router/Router.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+// import ErrorPage from "../pages/ErrorPage";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+    // errorElement:<ErrorPage></ErrorPage>
+  },
+]);
+export default router;
